b358720860a18f4fa5205ec61cf1171a8f94b3e7
Jex787 <jabirahamad96@gmail.com>
2023-08-25 21:36:34 +0600

    select-button-fixed

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,58 @@
-const selectBtnS = document.querySelectorAll('.select-btn');
+// const selectBtnS = document.querySelectorAll('.select-btn');
 
-let clickCounter = 0;
-selectBtnS.forEach(selectBtn => {
-    selectBtn.addEventListener('click', () => {
-        const player = document.getElementsByTagName('h3');
-        const playerName = player[clickCounter].innerText;
-        const display = document.getElementById('player-list');
-        const newLi = document.createElement('li');
-        let text = document.createTextNode(playerName);
-        newLi.appendChild(text);
-        display.appendChild(newLi);
-        clickCounter++;
-    });
-});
+// let clickCounter = 0;
+// selectBtnS.forEach(selectBtn => {
+//     selectBtn.addEventListener('click', () => {
+//         const player = document.getElementsByTagName('h3');
+//         const playerName = player[clickCounter].innerText;
+//         const display = document.getElementById('player-list');
+//         const newLi = document.createElement('li');
+//         let text = document.createTextNode(playerName);
+//         newLi.appendChild(text);
+//         display.appendChild(newLi);
+//         clickCounter++;
+//     });
+// });
+
+function setListItem(buttonID, divPos) {
+    let parentID = document.querySelectorAll('.player-name');
+    let playerName = parentID[divPos].firstElementChild.innerText;
+    let listNode = document.getElementById('player-list');
+
+    let lists = listNode.getElementsByTagName('li');
+    console.log(lists.length);
+    if (lists.length > 4) {
+        alert('You have already selected your top 5!');
+    }
+    else {
+        let liNode = document.createElement('li');
+        let txtNode = document.createTextNode(playerName);
+
+        liNode.appendChild(txtNode);
+        listNode.appendChild(liNode);
+
+        document.getElementById(buttonID).disabled = true;
+    }
+
+}
 
 document.getElementById('spend-per-player').addEventListener('click', function () {
     const perPlayerSpendField = document.getElementById('per-player-spend-field');
     const perPlayerSpendString = perPlayerSpendField.value;
     const perPlayerSpendValue = parseFloat(perPlayerSpendString);
 
-    const numberOfPlayer = document.getElementById("player-list").getElementsByTagName("li").length
+    const numberOfPlayer = document.getElementById("player-list").getElementsByTagName("li").length;
 
-    const total = perPlayerSpendValue * numberOfPlayer;
+    if (isNaN(perPlayerSpendString)) {
+        alert('Please provide a number!');
+    }
+    else {
 
-    const totalExpenceField = document.getElementById('total-expence-player');
-    totalExpenceField.innerText = total;
+        const total = perPlayerSpendValue * numberOfPlayer;
+
+        const totalExpenceField = document.getElementById('total-expence-player');
+        totalExpenceField.innerText = total;
+    }
 })
 
 document.getElementById('spend').addEventListener('click', function () {
@@ -40,8 +68,14 @@ document.getElementById('spend').addEventListener('click', function () {
     const spendOnCoachString = coachField.value;
     const spendOnCoachValue = parseFloat(spendOnCoachString);
 
-    const totalSpend = totalExpenceValue + spendOnManagerValue + spendOnCoachValue;
+    if (isNaN(totalExpenceString) || isNaN(spendOnManagerString) || isNaN(spendOnCoachString)) {
+        alert('Please provide a number!');
+    }
+    else {
 
-    const totalSpendField = document.getElementById('total-spend');
-    totalSpendField.innerText = totalSpend;
+        const totalSpend = totalExpenceValue + spendOnManagerValue + spendOnCoachValue;
+
+        const totalSpendField = document.getElementById('total-spend');
+        totalSpendField.innerText = totalSpend;
+    }
 })
